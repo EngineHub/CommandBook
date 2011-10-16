32b10f13bcd6b6a220ec0ac62a392e0fc604ebc7
zml2008 <zach@zachsthings.com>
2011-10-15 23:19:17 -0700

    Added commands to remove homes and warps

--- src/main/java/com/sk89q/commandbook/commands/HomeManagementCommands.java
+++ src/main/java/com/sk89q/commandbook/commands/HomeManagementCommands.java
@@ -18,6 +18,44 @@
 
 package com.sk89q.commandbook.commands;
 
+import com.sk89q.commandbook.CommandBookPlugin;
+import com.sk89q.commandbook.locations.NamedLocation;
+import com.sk89q.minecraft.util.commands.Command;
+import com.sk89q.minecraft.util.commands.CommandContext;
+import com.sk89q.minecraft.util.commands.CommandException;
+import com.sk89q.minecraft.util.commands.CommandPermissions;
+import org.bukkit.ChatColor;
+import org.bukkit.World;
+import org.bukkit.command.CommandSender;
+
 public class HomeManagementCommands {
+
+    @Command(
+            aliases = {"del", "delete", "remove", "rem"},
+            usage = "[homename] [world]",
+            desc = "Remove a home",
+            min = 0, max = 2 )
+    @CommandPermissions({"commandbook.home.remove"})
+    public static void remove(CommandContext args, CommandBookPlugin plugin,
+        CommandSender sender) throws CommandException {
+        World world;
+        String homeName = sender.getName();
+        if (args.argsLength() == 2) {
+            world = plugin.matchWorld(sender, args.getString(1));
+        } else {
+            world = plugin.checkPlayer(sender).getWorld();
+        }
+        if (args.argsLength() > 0) homeName = args.getString(0);
+        NamedLocation home = plugin.getHomesManager().get(world, homeName);
+        if (home == null) {
+            throw new CommandException("No home found for " + homeName + " in world " + world.getName());
+        }
+        if (!home.getCreatorName().equals(sender.getName())) {
+            plugin.checkPermission(sender, "commandbook.home.remove.other");
+        }
+
+        plugin.getHomesManager().remove(world, homeName);
+        sender.sendMessage(ChatColor.YELLOW + "Home for " + homeName + " removed.");
+    }
 
 }
--- src/main/java/com/sk89q/commandbook/commands/WarpManagementCommands.java
+++ src/main/java/com/sk89q/commandbook/commands/WarpManagementCommands.java
@@ -18,6 +18,42 @@
 
 package com.sk89q.commandbook.commands;
 
+import com.sk89q.commandbook.CommandBookPlugin;
+import com.sk89q.commandbook.locations.NamedLocation;
+import com.sk89q.minecraft.util.commands.Command;
+import com.sk89q.minecraft.util.commands.CommandContext;
+import com.sk89q.minecraft.util.commands.CommandException;
+import com.sk89q.minecraft.util.commands.CommandPermissions;
+import org.bukkit.ChatColor;
+import org.bukkit.World;
+import org.bukkit.command.CommandSender;
+
 public class WarpManagementCommands {
 
+    @Command(
+            aliases = {"del", "delete", "remove", "rem"},
+            usage = "<warpname> [world]",
+            desc = "Remove a warp",
+            min = 1, max = 2 )
+    @CommandPermissions({"commandbok.warp.remove"})
+    public static void remove(CommandContext args, CommandBookPlugin plugin,
+        CommandSender sender) throws CommandException {
+        World world;
+        String warpName = args.getString(0);
+        if (args.argsLength() == 2) {
+            world = plugin.matchWorld(sender, args.getString(1));
+        } else {
+            world = plugin.checkPlayer(sender).getWorld();
+        }
+        NamedLocation warp = plugin.getWarpsManager().get(world, warpName);
+        if (warp == null) {
+            throw new CommandException("No warp named " + warpName + " found for world " + world.getName());
+        }
+        if (!warp.getCreatorName().equals(sender.getName())) {
+            plugin.checkPermission(sender, "commandbook.warp.remove.other");
+        }
+
+        plugin.getWarpsManager().remove(world, warpName);
+        sender.sendMessage(ChatColor.YELLOW + "Warp " + warpName + " removed.");
+    }
 }
