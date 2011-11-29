4bac26a7c992ef10e30c3f3c82f45c66ebe8f181
sk89q <the.sk89q@gmail.com>
2011-11-29 09:33:18 -0800

    Minor cleanup.

--- src/main/java/com/sk89q/commandbook/locations/FlatFileLocationsManager.java
+++ src/main/java/com/sk89q/commandbook/locations/FlatFileLocationsManager.java
@@ -27,7 +27,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
