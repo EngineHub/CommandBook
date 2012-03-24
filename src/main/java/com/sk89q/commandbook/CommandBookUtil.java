89821aa1fddfba519894ea00f2b3ca0f0277c47d
zml2008 <zach@zachsthings.com>
2012-03-24 11:23:39 -0700

    Handle <1.2.4 Bukkit versions in CommandBookUtil.replaceColorMacros()

--- src/main/java/com/sk89q/commandbook/CommandBookUtil.java
+++ src/main/java/com/sk89q/commandbook/CommandBookUtil.java
@@ -96,12 +96,16 @@ public class CommandBookUtil {
         // use mojang's symbols where we can, make new ones up when they are already used
         str = str.replace("`k", ChatColor.MAGIC.toString());
 
-        str = str.replace("`l", ChatColor.BOLD.toString());
-        str = str.replace("`m", ChatColor.STRIKETHROUGH.toString());
-        str = str.replace("`n", ChatColor.UNDERLINE.toString());
-        str = str.replace("`o", ChatColor.ITALIC.toString());
+        try {
+            str = str.replace("`l", ChatColor.BOLD.toString());
+            str = str.replace("`m", ChatColor.STRIKETHROUGH.toString());
+            str = str.replace("`n", ChatColor.UNDERLINE.toString());
+            str = str.replace("`o", ChatColor.ITALIC.toString());
 
-        str = str.replace("`x", ChatColor.RESET.toString());
+            str = str.replace("`x", ChatColor.RESET.toString());
+        } catch (NoSuchFieldError t) {
+            // Keep this until 1.2.4+ is used on most Bukkit servers
+        }
 
         return str;
     }
