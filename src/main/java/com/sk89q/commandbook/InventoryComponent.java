a4d7c359722b32ce33ede12649d4c0a898d145f4
Dark Arc <theexcel@rocketmail.com>
2012-04-26 13:26:56 -0400

    Added the enchantments command again

--- src/main/java/com/sk89q/commandbook/InventoryComponent.java
+++ src/main/java/com/sk89q/commandbook/InventoryComponent.java
@@ -18,6 +18,7 @@
 
 package com.sk89q.commandbook;
 
+import com.sk89q.commandbook.commands.PaginatedResult;
 import com.sk89q.commandbook.util.PlayerUtil;
 import com.sk89q.minecraft.util.commands.Command;
 import com.sk89q.minecraft.util.commands.CommandContext;
@@ -31,10 +32,12 @@ import com.zachsthings.libcomponents.config.Setting;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Set;
 
@@ -436,6 +439,16 @@ public class InventoryComponent extends BukkitComponent {
             }
 
             player.sendMessage(ChatColor.YELLOW + "Items compacted into stacks!");
+        }
+
+        @Command(aliases = {"enchantments", "listenchant", "lsenchant"}, desc = "List available enchantments", usage = "[-p page]", flags = "p:")
+        public void enchantments(CommandContext args, CommandSender sender) throws CommandException {
+            new PaginatedResult<Enchantment>("Name - ID - Max Level") {
+                @Override
+                public String format(Enchantment entry) {
+                    return entry.getName() + " - " + entry.getId() + " - " + entry.getMaxLevel();
+                }
+            }.display(sender, Arrays.asList(Enchantment.values()), args.getFlagInteger('p', 1));
         }
     }
 }
