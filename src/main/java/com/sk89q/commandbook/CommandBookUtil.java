156ba8f71caefaa25c8c7678909ce7af0f51f240
wizjany <wizjany@gmail.com>
2013-08-20 16:21:18 -0400

    Updated /compass directions from the change ages ago.

--- src/main/java/com/sk89q/commandbook/CommandBookUtil.java
+++ src/main/java/com/sk89q/commandbook/CommandBookUtil.java
@@ -174,23 +174,23 @@ public class CommandBookUtil {
      */
     private static String getDirection(double rot) {
         if (0 <= rot && rot < 22.5) {
-            return "North";
-        } else if (22.5 <= rot && rot < 67.5) {
-            return "Northeast";
-        } else if (67.5 <= rot && rot < 112.5) {
-            return "East";
-        } else if (112.5 <= rot && rot < 157.5) {
-            return "Southeast";
-        } else if (157.5 <= rot && rot < 202.5) {
-            return "South";
-        } else if (202.5 <= rot && rot < 247.5) {
-            return "Southwest";
-        } else if (247.5 <= rot && rot < 292.5) {
             return "West";
-        } else if (292.5 <= rot && rot < 337.5) {
+        } else if (22.5 <= rot && rot < 67.5) {
             return "Northwest";
-        } else if (337.5 <= rot && rot < 360.0) {
+        } else if (67.5 <= rot && rot < 112.5) {
             return "North";
+        } else if (112.5 <= rot && rot < 157.5) {
+            return "Northeast";
+        } else if (157.5 <= rot && rot < 202.5) {
+            return "East";
+        } else if (202.5 <= rot && rot < 247.5) {
+            return "Southeast";
+        } else if (247.5 <= rot && rot < 292.5) {
+            return "South";
+        } else if (292.5 <= rot && rot < 337.5) {
+            return "Southwest";
+        } else if (337.5 <= rot && rot < 360.0) {
+            return "West";
         } else {
             return null;
         }
