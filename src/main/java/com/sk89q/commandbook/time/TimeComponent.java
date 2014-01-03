bca87188205d46a79fde111635d28a6b428d90f5
Dark Arc <theexcel@rocketmail.com>
2014-01-03 00:56:41 -0500

    Moved the method for parsing world time out of the Time Component and into CommandBookUtil
    * Additionally optimizes and cleans up some code in the Time Component
    * Deprecated method remains for API compatibility

--- src/main/java/com/sk89q/commandbook/time/TimeComponent.java
+++ src/main/java/com/sk89q/commandbook/time/TimeComponent.java
@@ -40,7 +40,6 @@ import org.bukkit.event.world.WorldUnloadEvent;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -48,11 +47,6 @@ import java.util.regex.Pattern;
  */
 @ComponentInformation(friendlyName = "Time Control", desc = "Commands to manage and lock time for a world.")
 public class TimeComponent extends BukkitComponent implements Listener {
-
-    /**
-     * A pattern that matches time given in 12-hour form (xx:xx(am|pm))
-     */
-    protected static final Pattern TWELVE_HOUR_TIME = Pattern.compile("^([0-9]+(?::[0-9]+)?)([apmAPM\\.]+)$");
 
     /**
      * A Map of time locker tasks for worlds.
@@ -87,7 +81,7 @@ public class TimeComponent extends BukkitComponent implements Listener {
                 int time = 0;
 
                 try {
-                    time = matchTime(String.valueOf(entry.getValue()));
+                    time = CommandBookUtil.matchMCWorldTime(String.valueOf(entry.getValue()));
                 } catch (CommandException e) {
                     CommandBook.logger().warning("Time lock: Failed to parse time '"
                             + entry.getValue() + "'");
@@ -177,84 +171,10 @@ public class TimeComponent extends BukkitComponent implements Listener {
      * @return
      * @throws CommandException
      */
-    public int matchTime(String timeStr) throws CommandException {
-        Matcher matcher;
+    @Deprecated
+    public static int matchTime(String timeStr) throws CommandException {
 
-        try {
-            int time = Integer.parseInt(timeStr);
-
-            // People tend to enter just a number of the hour
-            if (time <= 24) {
-                return ((time - 8) % 24) * 1000;
-            }
-
-            return time;
-        } catch (NumberFormatException e) {
-            // Not an integer!
-        }
-
-        // Tick time
-        if (timeStr.matches("^*[0-9]+$")) {
-            return Integer.parseInt(timeStr.substring(1));
-
-            // Allow 24-hour time
-        } else if (timeStr.matches("^[0-9]+:[0-9]+$")) {
-            String[] parts = timeStr.split(":");
-            int hours = Integer.parseInt(parts[0]);
-            int mins = Integer.parseInt(parts[1]);
-            return (int) (((hours - 8) % 24) * 1000
-                    + Math.round((mins % 60) / 60.0 * 1000));
-
-            // Or perhaps 12-hour time
-        } else if ((matcher = TWELVE_HOUR_TIME.matcher(timeStr)).matches()) {
-            String time = matcher.group(1);
-            String period = matcher.group(2);
-            int shift;
-
-            if (period.equalsIgnoreCase("am")
-                    || period.equalsIgnoreCase("a.m.")) {
-                shift = 0;
-            } else if (period.equalsIgnoreCase("pm")
-                    || period.equalsIgnoreCase("p.m.")) {
-                shift = 12;
-            } else {
-                throw new CommandException("'am' or 'pm' expected, got '"
-                        + period + "'.");
-            }
-
-            String[] parts = time.split(":");
-            int hours = Integer.parseInt(parts[0]);
-            int mins = parts.length >= 2 ? Integer.parseInt(parts[1]) : 0;
-            return (int) ((((hours % 12) + shift - 8) % 24) * 1000
-                    + (mins % 60) / 60.0 * 1000);
-
-            // Or some shortcuts
-        } else if (timeStr.equalsIgnoreCase("dawn")) {
-            return (6 - 8 + 24) * 1000;
-        } else if (timeStr.equalsIgnoreCase("sunrise")) {
-            return (7 - 8 + 24) * 1000;
-        } else if (timeStr.equalsIgnoreCase("morning")) {
-            return (24) * 1000;
-        } else if (timeStr.equalsIgnoreCase("day")) {
-            return (24) * 1000;
-        } else if (timeStr.equalsIgnoreCase("midday")
-                || timeStr.equalsIgnoreCase("noon")) {
-            return (12 - 8 + 24) * 1000;
-        } else if (timeStr.equalsIgnoreCase("afternoon")) {
-            return (14 - 8 + 24) * 1000;
-        } else if (timeStr.equalsIgnoreCase("evening")) {
-            return (16 - 8 + 24) * 1000;
-        } else if (timeStr.equalsIgnoreCase("sunset")) {
-            return (21 - 8 + 24) * 1000;
-        } else if (timeStr.equalsIgnoreCase("dusk")) {
-            return (21 - 8 + 24) * 1000 + (int) (30 / 60.0 * 1000);
-        } else if (timeStr.equalsIgnoreCase("night")) {
-            return (22 - 8 + 24) * 1000;
-        } else if (timeStr.equalsIgnoreCase("midnight")) {
-            return (0 - 8 + 24) * 1000;
-        }
-
-        throw new CommandException("Time input format unknown.");
+        return CommandBookUtil.matchMCWorldTime(timeStr);
     }
 
     public class Commands {
@@ -301,7 +221,7 @@ public class TimeComponent extends BukkitComponent implements Listener {
 
             if (!onlyLock) {
                 unlock(world);
-                world.setTime(matchTime(timeStr));
+                world.setTime(CommandBookUtil.matchMCWorldTime(timeStr));
             }
 
             String verb = "set";
@@ -384,17 +304,17 @@ public class TimeComponent extends BukkitComponent implements Listener {
                 return;
             }
 
+            int time = CommandBookUtil.matchMCWorldTime(timeStr);
+
             for (Player player : players) {
-                if (!player.equals(sender)) {
-                    player.sendMessage(ChatColor.YELLOW + "Your time set to " + CommandBookUtil.getTimeString(player.getPlayerTime()));
-                } else {
-                    player.sendMessage(ChatColor.YELLOW + "Your time set to " + CommandBookUtil.getTimeString(player.getPlayerTime()));
+                player.sendMessage(ChatColor.YELLOW + "Your time set to " + CommandBookUtil.getTimeString(player.getPlayerTime()));
+                if (player.equals(sender)) {
                     included = true;
                 }
-                player.setPlayerTime(args.hasFlag('w') ? Integer.parseInt(timeStr) : matchTime(timeStr), args.hasFlag('w'));
+                player.setPlayerTime(args.hasFlag('w') ? Integer.parseInt(timeStr) : time, args.hasFlag('w'));
             }
             if (!included) {
-                sender.sendMessage(ChatColor.YELLOW + "Player times set to " + CommandBookUtil.getTimeString(matchTime(timeStr)));
+                sender.sendMessage(ChatColor.YELLOW + "Player times set to " + CommandBookUtil.getTimeString(time));
             }
         }
     }
