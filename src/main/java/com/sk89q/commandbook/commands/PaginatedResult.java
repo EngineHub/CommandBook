28eb2d2e31e6dac7dd3c1ad3dea44526f5196836
Dark Arc <theexcel@rocketmail.com>
2014-05-03 03:22:51 -0400

    Made the PaginatedResult output feel much cleaner and more professional

--- src/main/java/com/sk89q/commandbook/commands/PaginatedResult.java
+++ src/main/java/com/sk89q/commandbook/commands/PaginatedResult.java
@@ -33,7 +33,7 @@ import java.util.List;
 public abstract class PaginatedResult<T> {
     private final String header;
 
-    protected static final int PER_PAGE = 6;
+    protected static final int PER_PAGE = 9;
     
     public PaginatedResult(String header) {
         this.header = header;
@@ -48,10 +48,16 @@ public abstract class PaginatedResult<T> {
         --page;
 
         int maxPages = results.size() / PER_PAGE;
-        if (page < 0 || page > maxPages) throw new CommandException(
-                "Unknown page selected! " + (maxPages + 1) + " total pages.");
 
-        sender.sendMessage(ChatColor.YELLOW + header + " (page " + (page + 1) + "/" + (maxPages + 1) + ")");
+        // If the content divides perfectly, eg (18 entries, and 9 per page)
+        // we end up with a blank page this handles this case
+        if (results.size() % PER_PAGE == 0) {
+            maxPages--;
+        }
+
+        page = Math.max(0, Math.min(page, maxPages));
+
+        sender.sendMessage(ChatColor.YELLOW + header + " - Page (" + (page + 1) + "/" + (maxPages + 1) + ")");
         for (int i = PER_PAGE * page; i < PER_PAGE * page + PER_PAGE  && i < results.size(); i++) {
             sender.sendMessage(ChatColor.YELLOW.toString() + format(results.get(i)));
         }
