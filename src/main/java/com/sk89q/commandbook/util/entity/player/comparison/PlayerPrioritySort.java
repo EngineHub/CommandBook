0fe600cfc3e1cdd5580573b42354a3ccc5f40c21
Wyatt Childers <theexcel@rocketmail.com>
2014-06-26 22:51:24 -0400

    Fixed a sorting regression

--- src/main/java/com/sk89q/commandbook/util/entity/player/comparison/PlayerPrioritySort.java
+++ src/main/java/com/sk89q/commandbook/util/entity/player/comparison/PlayerPrioritySort.java
@@ -14,17 +14,13 @@ public class PlayerPrioritySort implements Comparator<Player> {
 
     @Override
     public int compare(Player o1, Player o2) {
-        // If they are the same return such
-        if (o1.equals(o2)) {
-            return 0;
-        }
         // If the sender is player 1 return 1
         // to push it after player 2, otherwise
         // sort by name
         if (sender.equals(o1)) {
             return 1;
         } else {
-            return o1.getName().compareToIgnoreCase(o2.getName());
+            return o1.equals(o2) ? 0 : o1.getName().compareToIgnoreCase(o2.getName());
         }
     }
 }
