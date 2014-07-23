76a77227f2a63ede2efaaaf807fc83f83cafb8e1
Wyatt Childers <theexcel@rocketmail.com>
2014-07-23 14:07:28 -0400

    Began implementation of profile types

--- src/main/java/com/sk89q/commandbook/profiles/ProfileScope.java
+++ src/main/java/com/sk89q/commandbook/profiles/ProfileScope.java
@@ -3,5 +3,6 @@ package com.sk89q.commandbook.profiles;
 import java.io.File;
 
 public interface ProfileScope {
+    public ProfileType getType();
     public File getDir();
 }
---
+++ src/main/java/com/sk89q/commandbook/profiles/ProfileType.java
@@ -0,0 +1,7 @@
+package com.sk89q.commandbook.profiles;
+
+public enum ProfileType {
+    GLOBAL,
+    PERSONAL,
+    TAG
+}
